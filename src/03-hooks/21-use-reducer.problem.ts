fcdd2414581cb61e34adf41adbcaa671bebc2174
devgenx <sebastian110420@gmail.com>
2023-05-10 12:57:28 -0700

    finished

--- src/03-hooks/21-use-reducer.problem.ts
+++ src/03-hooks/21-use-reducer.problem.ts
@@ -1,7 +1,23 @@
 import { useReducer } from "react";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const reducer = (state: unknown, action: unknown) => {
+type State = {
+  state: { count: number };
+  action:
+    | {
+        type: "add";
+        add: number;
+      }
+    | {
+        type: "subtract";
+        subtract: number;
+      };
+};
+
+const reducer = (
+  state: { count: number },
+  action: { type: "add"; add: number } | { type: "subtract"; subtract: number }
+) => {
   switch (action.type) {
     case "add":
       return { count: state.count + action.add };
